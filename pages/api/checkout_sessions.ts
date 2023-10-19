f4b0b04ef4b40c07e87dd98c88ec6917477dd04b
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-19 18:14:32 +0200

    Configure env and also session

---
+++ pages/api/checkout_sessions.ts
@@ -0,0 +1,30 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+
+const handler = async(req: any, res: any) => {
+    if (req.method === 'POST'){
+        try{
+             // Create Checkout Sessions from body params.
+             const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price: 'price_1O2DAlDYGqu4gEhPJTmtBjkC',
+                        quantity: 1,
+                    }
+                ],
+                mode: 'payment',
+                success_url: `${req.headers.origin}/?success=true`,
+                cancel_url: `${req.headers.origin}/?canceled=true`,
+             });
+             res.redirect(303, session.url);
+
+        }
+        catch (err: any){
+            res.status(err.statusCode || 500).json(err.message);
+        }
+    }
+    else{
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
+}
